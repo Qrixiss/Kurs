39ce25908a873075ce0202a898ba1be91cc2df91
Qrixiss <avdushkinkirill835@gmail.com>
2024-12-19 00:06:49 +0300

    Add files via upload

--- send_order.php
+++ send_order.php
@@ -1,16 +1,17 @@
 <?php
-// Включение отображения ошибок
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $from = isset($_POST['from']) ? $_POST['from'] : 'Не указано';
     $to = isset($_POST['to']) ? $_POST['to'] : 'Не указано';
+    $name = isset($_POST['username']) ? $_POST['username'] : 'Не указано';
+    $phone = isset($_POST['phone']) ? $_POST['phone'] : 'Не указано';
 
-    $message = "Заказ такси:\nОткуда: $from\nКуда: $to";
+    $message = "Заказ такси:\nОткуда: $from\nКуда: $to\n Имя: $name\n Телефон: $phone";
 
-    $botToken = "6423951514:AAE848xYBRpAx92gihFxrnWyXr70-ULgev0";  // Замените на токен вашего бота
-    $chatId = "-1002372625214";  // Замените на ваш Chat ID
+    $botToken = "6423951514:AAE848xYBRpAx92gihFxrnWyXr70-ULgev0";
+    $chatId = "-1002372625214"; 
     $url = "https://api.telegram.org/bot$botToken/sendMessage";
 
     $data = [
